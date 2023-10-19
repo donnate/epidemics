eb2d330cfe81b0fbeeb16786f6e9b4682c3bdd75
Claire Donnat <cdonnat@midway3-login4.rcc.local>
2023-10-19 10:48:49 -0500

    fix merge

--- experiments/launch_experiments_pa.sh
+++ experiments/launch_experiments_pa.sh
@@ -7,12 +7,13 @@ beta_values="0.9 0.8 0.5 0.1 0.05"
 algorithms="y true_p"
 
 # Loop over the combinations and launch the sbatch command
-for algorithm in $algorithms; do
+#for algorithm in $algorithms; do
   for power in $power_values; do
     for diffuse in $diffuse_values; do
       for beta in $beta_values; do
-        sbatch experiments/simulation_pa.sh 1000 "$beta" 0.01 1 "$power" none 30 "$diffuse" "$algorithm"
+        #sbatch experiments/simulation_pa.sh 1000 "$beta" 0.01 1 "$power" none 30 "$diffuse" "$algorithm"
+        sbatch experiments/simulation_pa.sh 1000 "$beta" 0.01 1 "$power" none 30 "$diffuse" true_p
       done
     done
   done
-done
+#done
